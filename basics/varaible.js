af2d168fec1cd052dc67ef195e684af255963407
Fahad ul Hassan <54173716+fahadulhassan2@users.noreply.github.com>
2023-05-13 17:40:05 +0000

    TDZ and note updated

--- basics/varaible.js
+++ basics/varaible.js
@@ -8,7 +8,6 @@ let accountState;
 
 // accountId = 2 // not allowed
 
-
 accountEmail = "hc@hc.com"
 accountPassword = "21212121"
 
@@ -27,3 +26,16 @@ because of issue in block scope and functional scope
 
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+//  Note 
+
+//  Let ,const have block & local scope  and var have gobale scope
+
+// when we use const ,let varaible goes in TDZ Tempory Dead Zone
+
+// when we use var keywork varaible goes in hosting -> undefine
+
+//when we use const must decallar value other its throw error
+
+// e.g const age; -> error
+// e.g const age = 0; -> correct
